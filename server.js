74116713865076a138a6ddf05bb8d0a7bfff88e0
Alex <alexandra.casser@gmail.com>
2016-04-04 16:46:07 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/roamwildlife';
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/roam';
 var mongoose = require('mongoose');
 
 mongoose.connect(mongoUri);
@@ -13,12 +13,10 @@ app.use(express.static('public'));
 var sightingsController = require('./controllers/sightings.js');
 app.use('/sightings', sightingsController);
 
-mongoose.connect('mongodb://localhost:27017/roam');
-
 mongoose.connection.once('open', function(){
 		app.listen(3000, function(){
 		console.log('====================')
 		console.log('running on 3000')
 		console.log('====================')
-	})
-})
+	});
+});
