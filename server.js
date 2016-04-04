a3b2d267df2e422bcced0ff572da820a8639202e
Alex <alexandra.casser@gmail.com>
2016-04-04 14:22:57 -0400

    preparing to push to heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var app = express();
 var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/roamwildlife';
 var mongoose = require('mongoose');
